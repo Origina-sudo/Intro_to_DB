eeea5337b57f1482038517adbeceb010d41171cd
Origina-sudo <arthurba8585@gmail.com>
2024-07-28 22:41:26 +0000

    updates

--- task_4.sql
+++ task_4.sql
@@ -1,16 +1,2 @@
--- Switch to the correct database
 USE alx_book_store;
-
--- Query to get the full description of the 'books' table
-SELECT 
-    COLUMN_NAME AS 'Column Name', 
-    COLUMN_TYPE AS 'Column Type', 
-    IS_NULLABLE AS 'Is Nullable', 
-    COLUMN_KEY AS 'Column Key', 
-    COLUMN_DEFAULT AS 'Default Value', 
-    EXTRA AS 'Extra Info'
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books';
+SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE, COLUMN_TYPE, COLUMN_COMMENT FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books'; 
