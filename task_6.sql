800e38c8b83a6eba27d93ccdc591761b02b8957a
Origina-sudo <arthurba8585@gmail.com>
2024-07-28 22:23:41 +0000

    updates

---
+++ task_6.sql
@@ -0,0 +1,6 @@
+USE alx_book_store;
+
+INSERT INTO customers (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
